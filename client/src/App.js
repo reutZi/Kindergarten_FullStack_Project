1e5fbc7fb1714f0c7b7b8b6b7b70d2d24aac2a16
רעות צפלביץ <tzipilev@g.jct.ac.il>
2024-08-29 09:52:40 +0300

    new routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,28 @@ import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-route
 import Login from './components/Login';
 import AttendanceTeacher from './components/Teacher/AttendanceTeacher';
 import AttendanceParent from './components/Parent/AttendanceParent';
-import Welcome from './components/Parent/Welcome';
-import Navbar from './components/Navbar'; // Your Sidebar component
-import './style/App.css'; // Importing the Tailwind CSS styles
+import Navbar from './components/Navbar';
+import NoticeBoard from './components/NoticeBoard';
+import ChooseKid from './components/Parent/ChooseKid';
+import WhatsNew from './components/WhatsNew';
+import './style/App.css';
 
 function AppContent() {
   const location = useLocation();
+  const user = JSON.parse(localStorage.getItem('user'));
   const isLoginPage = location.pathname === '/'; // Check if it's the login page
 
   return (
     <div className="main-container">
-     {!isLoginPage && <Navbar className="sidebar" />} {/* Sidebar component */}
+     {!isLoginPage && <Navbar className="sidebar" />}
       <div className="content">
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="parent/attendance" element={<AttendanceParent />} />
           <Route path="teacher/attendance" element={<AttendanceTeacher />} />
-          <Route path="parent/welcome" element={<Welcome />} />
+          <Route path="parent/choosekid" element={<ChooseKid />} />
+          <Route path="noticeboard" element={<NoticeBoard user={user}/>} />
+          <Route path="whatsnew" element={<WhatsNew user={user}/>} />
         </Routes>
       </div>
     </div>
